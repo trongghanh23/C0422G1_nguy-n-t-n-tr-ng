0d711ca0fb6f7f9c13f266c2a70dd75bf2ed834a
trongghanh23 <truongggh1602@gmail.com>
2022-06-20 20:19:14 +0700

    nộp bài tập

---
+++ src/s11_stack_queue/practice/Node.java
@@ -0,0 +1,14 @@
+package s11_stack_queue.practice;
+
+import java.util.Stack;
+
+public class Node {
+    public int key;
+    public Node next;
+
+    public Node(int key) {
+        this.key = key;
+        this.next = null;
+    }
+
+}
